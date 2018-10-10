0f8fc1da52b5bf9a68d1cca667d4f42c320d27b2
nikola.quantox <nikola.jeremic@quantox.com>
2018-10-10 16:34:21 +0200

    animaton & responsiveness

--- src/app/animated-blocks/animated-blocks.component.ts
+++ src/app/animated-blocks/animated-blocks.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimatedBlocksComponent implements OnInit {
 
+  name = 'Nikola Jeremić';
+  position = 'Web developer';
+  phone = '(+381) 69 1987 018';
+  mail = 'nikola87kg@gmail.com';
   constructor() { }
 
   ngOnInit() {
+    setInterval( () => {
+      this.changeData();
+    }, 5000);
+  }
+
+  changeData() {
+      if (this.name === 'Kristina Jeremić') {
+        this.name = 'Nikola Jeremić';
+        this.position = 'Web developer';
+        this.phone = '(+381) 69 1987 018';
+        this.mail = 'nikola87kg@gmail.com';
+      } else {
+        this.name = 'Kristina Jeremić';
+        this.position = 'Content Writer & Web Designer';
+        this.phone = '(+381) 69 534 02 92';
+        this.mail = 'zivanovic.v.kristina@gmail.com';
+      }
   }
 
 }
